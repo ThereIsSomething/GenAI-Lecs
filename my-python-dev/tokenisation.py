ee5847d08b0126f59e85e6a5a6b7a2cc900da3ad
ThereIsSomething <nitishbhambu07022005@gmail.com>
2026-04-29 01:11:35 +0530

    tokenisation.py

--- my-python-dev/tokenisation.py
+++ my-python-dev/tokenisation.py
@@ -1,19 +1,21 @@
 import tiktoken
-def count_tokens(text: str, model: str = "gpt-3.5-turbo") -> int:
-    """
-    Counts the number of tokens in a given text using the specified model's tokenizer.
 
-    Args:
-        text (str): The input text to be tokenized and counted.
-        model (str): The name of the model whose tokenizer to use. Default is "gpt-3.5-turbo".
+words = ['pencil','apple','me ','no no no','谢谢']
 
-    Returns:
-        int: The number of tokens in the input text.
-    """
-    # Get the appropriate encoding for the specified model
-    encoding = tiktoken.encoding_for_model(model)
-    
-    # Encode the text and return the number of tokens
-    return len(encoding.encode(text))
+encoding = tiktoken.encoding_for_model("gpt-4o-mini")
+newstr = "Hey, Hi! how are you doing nitish?"
 
-print(count_tokens("Hello, how are you?", model="gpt-3.5-turbo"))
+for i in words:
+    result = encoding.encode(i)
+    print(result)
+
+
+vars = (encoding.encode(newstr))
+print (vars)
+print(encoding.decode(vars))
+
+num_tokens = len(vars)
+print(num_tokens)
+for i in vars:
+    token_bytes = encoding.decode_single_token_bytes(i)
+    print(f"token bytes: {token_bytes}")
